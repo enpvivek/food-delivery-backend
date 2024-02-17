a27027f86784f5ff1f03a91ec93e2bbb697404ee
enpvivek <enpvivek@gmail.com>
2024-02-17 09:37:14 +0530

    fix: add home route

---
+++ data/home.js
@@ -0,0 +1,12 @@
+const home = {
+  statusCode: 200,
+  methods: {
+    "/bestPlaces": "returns best Places",
+    "/nearby": "returns nearby Places",
+    "/restaurant": "returns top restaurants",
+    "/restaurants2": "returns restaurants list",
+    "/suggestions": "returns suggestions",
+  },
+};
+
+module.exports = home;
--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const bestPlaces = require("./data/bestplaces")
-const nearbyPlaces = require("./data/nearby")
-const restaurant = require("./data/restaurant")
-const restaurants2 = require("./data/restaurants2")
-const suggestions = require("./data/suggestions")
+const bestPlaces = require("./data/bestplaces");
+const nearbyPlaces = require("./data/nearby");
+const restaurant = require("./data/restaurant");
+const restaurants2 = require("./data/restaurants2");
+const suggestions = require("./data/suggestions");
+const home = require("./data/home");
 
+const app = express();
 
-const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
+app.get("/", (req, res) => {
+  res.status(200).send(home);
+});
 
 app.get("/bestPlaces", (req, res) => {
-    res.status(200).send(bestPlaces)
-})
+  res.status(200).send(bestPlaces);
+});
 
 app.get("/nearbyPlaces", (req, res) => {
-    res.status(200).send(nearbyPlaces)
-})
+  res.status(200).send(nearbyPlaces);
+});
 
 app.get("/restaurant", (req, res) => {
-    res.status(200).send(restaurant)
-})
+  res.status(200).send(restaurant);
+});
 
 app.get("/restaurants2", (req, res) => {
-    res.status(200).send(restaurants2)
-})
+  res.status(200).send(restaurants2);
+});
 
 app.get("/suggestions", (req, res) => {
-    res.status(200).send(suggestions)
-})
+  res.status(200).send(suggestions);
+});
 
-
-const PORT = 8000
+const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+  const url = `http://localhost:${PORT}`;
+  console.log(`Server running at: ${url}`);
+});
